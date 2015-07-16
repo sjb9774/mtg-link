ab21e589bb24e65143e4a72c3cd884afda921e35
Stephen Biston <stephen@britecore.com>
2015-07-15 19:40:49 -0500

    remove pudb

--- mtg_link/DATA/__init__.py
+++ mtg_link/DATA/__init__.py
@@ -7,7 +7,6 @@ def _get_raw_card_data(*sets):
     with open(os.path.abspath(os.path.dirname(mtg_link.__file__)) + '/DATA/all_sets.json', 'r') as cards_file:
         # it's a single-line JSON file
         CARD_DATA = json.loads(unicode(cards_file.readline().replace('−', '-').replace('∞', 'infinity'), 'utf8').encode('utf-8'))
-    import pudb; pudb.set_trace()
     if sets:
         return {kw: arg for kw, arg in CARD_DATA.iteritems() if kw in sets}
     else:
